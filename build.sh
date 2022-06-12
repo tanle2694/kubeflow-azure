8b06517a6491f0ecffc11d98b1d4c421a2d444ea
tanle2694 <tanle2694@gmail.com>
2022-06-12 09:07:57 +0700

    Fix build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 if [ -d "manifests" ]
 then
     echo "manifests folder exists. It will be remove now"
-    rm -rf manifest
+    rm -rf manifests
 fi
 
 echo "Clone kubeflow manifest repo"
@@ -15,10 +15,16 @@ git clone https://github.com/kubeflow/manifests
 
 echo "Setup azure config env"
 # Copy azure config env to manifest repo
-cp -r azure-with-mysql manifest/apps/pipeline/upstream/env
+cp -r azure-with-mysql manifests/apps/pipeline/upstream/env
+
 
 echo "Setup kustomization"
-cp kustomizations/kubeflow-azure-mysql-customization/kustomization.yaml manifest/build
+mkdir manifests/build
+cp kustomizations/kubeflow-azure-mysql-customization/kustomization.yaml manifests/build
 
 
-while ! kustomize build manifest/build | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+while ! kustomize build manifests/build | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+
+kubectl patch svc istio-ingressgateway -p '{"spec": {"type": "LoadBalancer"}}' -n istio-system
+kubectl patch destinationrule ml-pipeline -p '{"spec" :{"trafficPolicy" :{"tls" :{"mode" :"DISABLE"}}}}' -n kubeflow --type=merge
+kubectl patch destinationrule ml-pipeline-ui -p '{"spec" :{"trafficPolicy" :{"tls" :{"mode" :"DISABLE"}}}}' -n kubeflow --type=merge
