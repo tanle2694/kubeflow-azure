421329ffaefae9475005d40e7d823948e93ab6d1
tanle2694 <tanle2694@gmail.com>
2022-06-15 11:45:33 +0700

    add modelmesh config

--- modelmesh.sh
+++ modelmesh.sh
@@ -3,7 +3,15 @@ RELEASE=release-0.8
 git clone -b $RELEASE --depth 1 --single-branch https://github.com/kserve/modelmesh-serving.git
 cd modelmesh-serving
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
-
-sed -i 's/kustomize/.\/kustomize/g' scripts/install.sh
+export PATH=$(pwd):$PATH
 kubectl create namespace modelmesh-serving
+rm modelmesh-serving/config/runtimes/mlserver-0.x.yaml
+cp model-mesh-config/mlserver-0.x.yaml modelmesh-serving/config/runtimes/
+
+
 ./scripts/install.sh --namespace modelmesh-serving --quickstart
+
+kubectl get secret storage-config -o yaml | sed 's/namespace: .*/namespace: kubeflow/' | kubectl apply -f -
+kubectl get secret storage-config -o yaml | sed 's/namespace: .*/namespace: kubeflow-user-example-com/' | kubectl apply -f -
+
+kubectl apply -f model_mesh_rbac.yaml
